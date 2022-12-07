cd2eb9471bd8813d9373892588096f49aae0c21a
hamid19471 <hamid.19471@gmail.com>
2022-12-07 19:12:21 +0330

    add more style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
-import "./App.css";
+import TodoList from "./components/TodoList/TodoList";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
+            <h1 className="font-bold text-gray-700 text-center text-2xl mb-5">
+                Todo Application
+            </h1>
+            <TodoList />
+        </div>
+    );
 }
 
 export default App;
